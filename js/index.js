76b50bf787a566dca6e4e6c82c59e1867efa36dc
Hemanth Rao <67952661+HemanthRa0@users.noreply.github.com>
2021-05-24 18:08:55 +0530

    optimised

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const render = function(data){
-    const content = document.getElementById('content');
     
+    const content = document.getElementById('content');
+
+    document.getElementById("content").innerHTML="";
+    
+    if(!data.sessions.length){
+        const div = document.createElement('div');
+        div.className = 'notava';
+        var emoji = String.fromCodePoint(0x1F622)
+        div.innerText = "Not Available"+emoji;
+        content.appendChild(div);
+    }
+
     for(let center of data.sessions){
 
         const br_begin_div = document.createElement('br');
@@ -21,22 +32,33 @@ const render = function(data){
         address_div.className = 'address';
         address_div.innerText = center.address;
 
+        const vaccine_text = document.createElement('div');
+        vaccine_text.className = 'vaccinetext';
+        vaccine_text.innerText = 'Vaccine Availability';
+
         const vaccine_div = document.createElement('div');
         vaccine_div.className = 'vaccine';
         vaccine_div.innerText = center.vaccine;
 
         const dose1_div = document.createElement('div');
         dose1_div.className = 'dose1';
-        dose1_div.innerText = center.available_capacity_dose1;
+        dose1_div.innerText = 'Dose1: '+center.available_capacity_dose1;
 
         const dose2_div = document.createElement('div');
         dose2_div.className = 'dose2';
-        dose2_div.innerText = center.available_capacity_dose2;
+        dose2_div.innerText = 'Dose2: '+center.available_capacity_dose2;
+
+        const age_text = document.createElement('div');
+        age_text.className = 'agetext';
+        age_text.innerText = 'Age';
 
         const age_div = document.createElement('div');
         age_div.className = 'age';
-        const buff = 'Age : '
-        age_div.innerText = buff+center.min_age_limit;
+        age_div.innerText = center.min_age_limit+'+';
+
+        const fee_text = document.createElement('div');
+        fee_text.className = 'feetext';
+        fee_text.innerText = 'Fee';
 
         const fee_div = document.createElement('div');
         fee_div.className = 'fee';
@@ -44,23 +66,26 @@ const render = function(data){
 
         const from_div = document.createElement('div');
         from_div.className = 'from';
-        from_div.innerText = center.from;
+        from_div.innerText = 'from_'+center.from;
 
         const to_div = document.createElement('div');
         to_div.className = 'to';
-        to_div.innerText = center.to;
+        to_div.innerText = 'to_'+center.to;
         
         const br_end = document.createElement('br');
 
         const hr_div = document.createElement('hr');
 
-        div.appendChild(state_div);
-        div.appendChild(district_div);
         div.appendChild(address_div);
+        div.appendChild(district_div);
+        div.appendChild(state_div);
+        div.appendChild(vaccine_text);
         div.appendChild(vaccine_div);
         div.appendChild(dose1_div);
         div.appendChild(dose2_div);
+        div.appendChild(age_text);
         div.appendChild(age_div);
+        div.appendChild(fee_text);
         div.appendChild(fee_div);
         div.appendChild(from_div);
         div.appendChild(to_div);
@@ -83,6 +108,21 @@ const fetchDetails = function(){
     const unformattedDate = document.getElementById('input_date').value;
     const date = unformattedDate.split('-').reverse().join('-');
     fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${date}`).then(parseJson).then(render);
+
+    fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${date}`)
+    .then(function(response){
+        console.log(response.status);
+        if(response.status !== 200){
+            console.log("Something went wrong!");
+            const content = document.getElementById('content');
+
+            const div = document.createElement('div');
+            div.className = 'incorrect';
+            var emoji = String.fromCodePoint(0x1F637)
+            div.innerText = "Please check your inputs and try agian"+emoji;
+            content.appendChild(div);
+        }
+    });
 };
 
 const setup = function(){

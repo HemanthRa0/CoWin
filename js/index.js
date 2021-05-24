a6c9db232feaf82411d5ba58d579cd266d18dd21
Hemanth Rao <67952661+HemanthRa0@users.noreply.github.com>
2021-05-24 19:06:25 +0530

    added rotation prompt

--- js/index.js
+++ js/index.js
@@ -131,9 +131,11 @@ const setup = function(){
 };
 
 window.onload = setup;
+window.onload = function(){
 window.addEventListener("orientationchange", function(event) {
     console.log("the orientation of the device is now " + event.target.screen.orientation.angle);
     if(event.target.screen.orientation.angle === 0){
         this.alert("Please rotate your device");
     }
   });
+};
